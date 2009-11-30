12115be75537e3180cadcb48b891f189ae1fbc30
mudge <mudge@mudge.name>
2009-11-30 09:31:02 -0700

    Add headers for Internet Explorer (particularly over HTTPS).

--- lib/action_view/template_handlers/prawn.rb
+++ lib/action_view/template_handlers/prawn.rb
@@ -6,10 +6,16 @@ module ActionView
       def compile(template)
         %Q{
           controller.response.content_type ||= Mime::PDF
+          controller.headers['Expires'] = '0'
+          controller.headers['Pragma'] = 'cache'
+          controller.headers['Cache-Control'] = 'private'
+          controller.headers['Accept-Ranges'] = 'bytes'
           source = ::Prawn::Document.new do |pdf|
             #{template.source}
           end
-          source.render
+          result = source.render
+          controller.headers['Content-Length'] = result.bytesize.to_s
+          result
         }
       end
     end
